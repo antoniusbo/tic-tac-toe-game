0e97d6161c52165a6a7d919e6a71015fc0eb4fbd
Anton Bolotin <antoniobolotin001@gmail.com>
2024-02-13 20:36:21 +0200

    Creating the files

---
+++ todolist.py
@@ -0,0 +1,6 @@
+# TODO: Two players to setup.
+#       A question to start a game.
+#       An empty grid and a request for the player1 to make a step.
+#       A grid with the output from player1 and a request for the player2 to make a step.
+#       Check if no win or lose.
+#       If someone wins then game over and a question to play again.
