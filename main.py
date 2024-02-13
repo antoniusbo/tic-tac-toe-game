88b947c5cdbfcc42958eb2b36f417e2d183e70ce
Anton Bolotin <antoniobolotin001@gmail.com>
2024-02-13 20:57:32 +0200

    Creating the files

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+while True:
 
-theBoard = {'7': ' ', '8': ' ', '9': ' ',
-            '4': ' ', '5': ' ', '6': ' ',
-            '1': ' ', '2': ' ', '3': ' '}
+    start_question = input("Would you like to play a game? Please respond 'Yes' or 'No': ")
+    if start_question.lower() == "yes":
+        print("Let's start it!")
+    elif start_question.lower() == "no":
+        print("The game is over.")
+    else:
+        print("Please respond 'Yes' or 'No'.")
 
-for key in theBoard:
-    board_keys.append(key)
+
+    theBoard = {'7': ' ', '8': ' ', '9': ' ',
+                '4': ' ', '5': ' ', '6': ' ',
+                '1': ' ', '2': ' ', '3': ' '}
+
+    for key in theBoard:
+        board_keys.append(key)
 
 
-def printBoard(board):
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
-    print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    def printBoard(board):
+        print(board['7'] + '|' + board['8'] + '|' + board['9'])
+        print('-+-+-')
+        print(board['4'] + '|' + board['5'] + '|' + board['6'])
+        print('-+-+-')
+        print(board['1'] + '|' + board['2'] + '|' + board['3'])
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-start_question = input("Would you like to play a game? Please respond 'Yes' or 'No': ")
-if start_question.lower() == "yes":
-    print("Let's start it!")
-elif start_question.lower() == "no":
-    print("The game is over.")
-else:
-    print("Please respond 'Yes' or 'No'.")
+while True:
+
+    start_question = input("Would you like to play a game? Please respond 'Yes' or 'No': ")
+    if start_question.lower() == "yes":
+        print("Let's start it!")
+    elif start_question.lower() == "no":
+        print("The game is over.")
+    else:
+        print("Please respond 'Yes' or 'No'.")
