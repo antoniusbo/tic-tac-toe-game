b495b5a5c183222682537da57e2abd90f6643400
Anton Bolotin <antoniobolotin001@gmail.com>
2024-02-13 21:05:14 +0200

    Creating the files

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-while True:
+game = 1
+
+while game == True:
 
     start_question = input("Would you like to play a game? Please respond 'Yes' or 'No': ")
     if start_question.lower() == "yes":
         print("Let's start it!")
     elif start_question.lower() == "no":
         print("The game is over.")
+        game = False
+
     else:
         print("Please respond 'Yes' or 'No'.")
