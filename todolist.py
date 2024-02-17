f1ab673332ca6ac2677f9da03d26a3c14e1e3a38
Anton Bolotin <antoniobolotin001@gmail.com>
2024-02-17 14:03:15 +0200

    Creating the files

--- todolist.py
+++ todolist.py
@@ -3,4 +3,5 @@
 #       An empty grid and a request for the player1 to make a step.
 #       A grid with the output from player1 and a request for the player2 to make a step.
 #       Check if no win or lose.
+#       Check the draw if 9 steps have been used in total.
 #       If someone wins then game over and a question to play again.
